98f95b04f3dacfc1b47c731fdf076b8e7fbb23f4
miguelS <msaldana.mdz@gmail.com>
2026-02-17 22:37:18 -0300

    v1.3 (dev) agrego 2 parametros mas

--- principal.py
+++ principal.py
@@ -1,5 +1,12 @@
 from funciones import multiplicar
-from parametros import url
+from parametros import url,ambiente,BD
+
+
+# Mostrar parámetros de configuración
+print("URL:", url)
+print("Ambiente:", ambiente)
+print("BD:", BD)
+
 
 # 1) Solicita dos números al usuario
 num1 = float(input("Ingresa el primer número: "))
@@ -8,6 +15,3 @@ num2 = float(input("Ingresa el segundo número: "))
 # 2) Multiplica usando la función encapsulada e imprime el resultado
 resultado = multiplicar(num1, num2)
 print("El resultado de la multiplicación es:", resultado)
-
-
-print("URL configurada:", url)
